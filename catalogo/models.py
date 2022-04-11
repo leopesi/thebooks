0b11a8886722d336622b0285fe904689d505aa50
Leonardo <leopesi@yahoo.com.br>
2022-04-11 18:47:21 -0300

    added models

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Genre(models.Model):
+    """Model representando um gênero de livro."""
+    name = models.CharField(max_length=70, help_text='Insira um gênero de livro (ex. Romance)')
+
+    def __str__(self):
+        """Retorna uma String representando o Model object."""
+        return self.name
+
+class Book(models.Model):
+    """Model representando o livro."""
+    title = models.CharField(max_length=70)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text='Descreva, com poucas palavras, o livro.')
+    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
+
+    def __str__(self):
+        """Retorna uma String representando o Model object."""
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+
+
+class Author(models.Model):
+    """Model representando o Autor."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Retorna uma url de acesso a um autor específico."""
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
--- thebooks/settings.py
+++ thebooks/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 SECRET_KEY = 'django-insecure-+_iesghk-168i%6ei11@1eo!ivbg#m664k+s+qx3q1ifmfj)&*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_MODE')
 
 ALLOWED_HOSTS = []
 
