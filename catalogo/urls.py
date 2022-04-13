c4284945fcf017b0d568be4fd87682a8f1a73b21
Leonardo <leopesi@yahoo.com.br>
2022-04-13 07:30:03 -0300

    added urls update/delete

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, BookListView, AuthorListView, BookDetailView, AuthorDetailView
-
+from .views import index, BookListView, BookDetailView, BookCreate, BookUpdate, BookDelete, AuthorListView, AuthorDetailView, AuthorCreate, AuthorUpdate, AuthorDelete
 urlpatterns = [
+
     path('/', index, name='index'),
+
     path('books/', BookListView.as_view(), name='books'),
     path('book/<int:pk>', BookDetailView.as_view(), name='book-detail'),
+    path('book/create/', BookCreate.as_view(), name='book-create'),
+    path('book/<int:pk>/update/', BookUpdate.as_view(), name='book-update'),
+    path('book/<int:pk>/delete/', BookDelete.as_view(), name='book-delete'),
+
     path('authors/', AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', AuthorDetailView.as_view(), name='author-detail'),
+    path('author/create/', AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', AuthorDelete.as_view(), name='author-delete'),
 ]
